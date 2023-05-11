a0e5826b1fe71f831dae49ade76c061a8d7c472a
manuelnones <manuelnones99@yahoo.com>
2023-05-11 15:38:17 +0200

    add table migration

---
+++ database/migrations/2023_05_11_125343_create_trains_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('Azienda');
+            $table->string('Stazione di partenza', 100);
+            $table->string('Stazione di arrivo', 100)->nullable();
+            $table->time('Orario di partenza', $precision = 4)->nullable();
+            $table->time('Orario di arrivo', $precision = 4)->nullable();
+            $table->mediumInteger('Codice Treno');
+            $table->tinyInteger('Numero Carrozze');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
